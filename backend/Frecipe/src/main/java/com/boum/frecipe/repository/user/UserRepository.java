674252dad57a5af572535bcc2b263adbfb06a651
yeoro <dufgh1009@naver.com>
2020-10-20 17:48:30 +0900

    S03P31D204-92 [+] Create UserService & UserServiceImpl

--- backend/Frecipe/src/main/java/com/boum/frecipe/repository/user/UserRepository.java
+++ backend/Frecipe/src/main/java/com/boum/frecipe/repository/user/UserRepository.java
@@ -1,8 +1,9 @@
-package com.boum.frecipe.repository;
+package com.boum.frecipe.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.boum.frecipe.domain.User;
+import com.boum.frecipe.domain.user.User;
+
 
 public interface UserRepository extends JpaRepository<User, Long>{
 }
---
+++ backend/Frecipe/src/main/java/com/boum/frecipe/service/user/UserService.java
@@ -0,0 +1,9 @@
+package com.boum.frecipe.service.user;
+
+import com.boum.frecipe.domain.user.User;
+
+public interface UserService {
+	
+	// 회원가입
+	public User signUp(User user);
+}
