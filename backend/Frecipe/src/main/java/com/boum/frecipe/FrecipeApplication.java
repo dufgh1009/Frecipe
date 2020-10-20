1065a57becef8a2faae3a2183f44e3fd883dc1d0
yeoro <dufgh1009@naver.com>
2020-10-20 16:00:12 +0900

    S03P31D204-83 [+] Create Spring Boot PJT

---
+++ backend/Frecipe/src/main/java/com/boum/frecipe/FrecipeApplication.java
@@ -0,0 +1,13 @@
+package com.boum.frecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrecipeApplication.class, args);
+	}
+
+}
