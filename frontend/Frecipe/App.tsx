431dc80423baf9fcaa210b33ab1f2ebd0b724d96
엄강우 <yhnb33@gmail.com>
2020-10-21 18:02:17 +0900

    S03P31D204-74[+] add bottom navigation

--- frontend/Frecipe/App.tsx
+++ frontend/Frecipe/App.tsx
@@ -8,13 +8,31 @@ import Main from "./navigation/Main";
 
 // const store = createStore(rootReducer, applyMiddleware(thunk));
 
-const App = () => {
-  return (
-    // <Provider store={store}>
-    //   <Main />
-    // </Provider>
-    <Main></Main>
-  );
-};
+// const App = () => {
+//   return (
+//     // <Provider store={store}>
+//     //   <Main />
+//     // </Provider>
+//     <Main></Main>
+//   );
+// };
 
-export default App;
+// export default App;
+
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// import Gate from './screens/Gate';
+
+export default function App() {
+  return <Gate />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
