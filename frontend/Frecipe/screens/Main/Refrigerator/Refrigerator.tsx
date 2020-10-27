f7f586c0af10d1cfd960a02d47eda19ac82b6299
엄강우 <yhnb33@gmail.com>
2020-10-27 17:23:48 +0900

    solve merge

--- frontend/Frecipe/screens/Main/Refrigerator/Refrigerator.tsx
+++ frontend/Frecipe/screens/Main/Refrigerator/Refrigerator.tsx
@@ -1,33 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import { Text, View } from 'react-native';
-import { Header, Overlay, Button } from 'react-native-elements'
-import { AntDesign, Entypo } from '@expo/vector-icons';
-import DropDownPicker from 'react-native-dropdown-picker';
-import SearchBar from "react-native-dynamic-search-bar/lib/SearchBar";
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-
-interface RefrigeratorProps { }
-interface RefrigeratorState {
-  filter: string,
-  addVisible: boolean,
-}
-
-class Refrigerator extends Component<RefrigeratorProps, RefrigeratorState> {
-  state = {
-    filter: 'eod',
-    addVisible: false,
-  }
-  addOverlay = () => {
-    this.setState({ addVisible: !this.state.addVisible });
-  };
-  render() {
-    const { addVisible } = this.state
-    return (
-      <View style={{ flex: 1, flexDirection: 'column' }}>
-        <KeyboardAwareScrollView>
-          <Header style={{ flex: 1 }}
-=======
 import { Text, View, StyleSheet, ScrollView, TextInput } from 'react-native';
 import { bindActionCreators } from "redux";
 import { Header, Overlay, Button, Input } from 'react-native-elements'
@@ -165,49 +136,16 @@ class Refrigerator extends Component<RefrigeratorProps, RefrigeratorState> {
         }}>
         <View style={styles.container}>
           <Header style={styles.header}
->>>>>>> feature/S03P31D204-110/refrigerator_add_modal
             backgroundColor='#00BD75'
             centerComponent={{ text: 'MY Refrigerator', style: { color: '#fff' } }}
             rightComponent={<AntDesign name="edit" size={24} color="white" />}
           />
-<<<<<<< HEAD
-          <View style={{ flex: 1, margin: 10 }}>
-=======
 
           <View style={styles.searchBar}>
->>>>>>> feature/S03P31D204-110/refrigerator_add_modal
             <SearchBar
               placeholder="재료를 검색하세요..."
             />
           </View>
-<<<<<<< HEAD
-          <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', margin: 4 }}>
-            <View style={{ flex: 4, borderColor: '#dedcdc', borderWidth: 1, borderRadius: 3, paddingHorizontal: 10, paddingVertical: 5, backgroundColor: 'white', flexDirection: 'row', justifyContent: 'space-between', marginLeft: 10 }}>
-              <Text style={{ color: '#EFB700', margin: 3 }}>유통기한 임박</Text>
-              <Text style={{ color: 'black', margin: 3 }}>1개</Text>
-            </View>
-            <View style={{ flex: 4, borderColor: '#dedcdc', borderWidth: 1, borderRadius: 3, paddingHorizontal: 10, paddingVertical: 5, backgroundColor: 'white', flexDirection: 'row', justifyContent: 'space-between', marginRight: 10 }}>
-              <Text style={{ color: 'red', margin: 3 }}>유통기한 만료</Text>
-              <Text style={{ color: 'black', margin: 3 }}>2개</Text>
-            </View>
-          </View>
-          <View style={{ flex: 10 }}>
-            <View style={{ flex: 1, flexDirection: 'row', marginHorizontal: 10, alignItems: "center", borderBottomWidth: 1, borderBottomColor: '#dedcdc', padding: 10 }}>
-              <View style={{ flex: 1.5, flexDirection: 'row', justifyContent: 'space-around', alignItems: "center" }}>
-                <Text>품목</Text>
-                <Text>5개</Text>
-              </View >
-              <View style={{ flex: 0.5 }}>
-              </View>
-              <View style={{ flex: 5 }}>
-                <DropDownPicker
-                  defaultValue={this.state.filter}
-                  items={[
-                    { label: '유통기한 임박 순', value: 'eod' },
-                    { label: '????', value: '??' },
-                  ]}
-                  containerStyle={{ height: 40 }}
-=======
           <View style={styles.expirationBar}>
             <View style={styles.expirationsBarSub}>
               <Text style={styles.expirationsBarSubYellow}>유통기한 임박</Text>
@@ -237,7 +175,6 @@ class Refrigerator extends Component<RefrigeratorProps, RefrigeratorState> {
                     { label: '업데이트 순', value: 'update' },
                   ]}
                   containerStyle={{ height: 30 }}
->>>>>>> feature/S03P31D204-110/refrigerator_add_modal
                   style={{ backgroundColor: '#fafafa' }}
                   itemStyle={{
                     justifyContent: 'flex-start'
@@ -245,19 +182,6 @@ class Refrigerator extends Component<RefrigeratorProps, RefrigeratorState> {
                   dropDownStyle={{ backgroundColor: '#fafafa' }}
                 />
               </View>
-<<<<<<< HEAD
-              <View style={{ flex: 2, flexDirection: 'row', justifyContent: 'flex-end' }}>
-                <Button type="clear" icon={<AntDesign name="plus" size={24} color="black" />} onPress={this.addOverlay}></Button>
-              </View>
-            </View>
-            <View style={{ flex: 9, flexDirection: 'column', marginHorizontal: 10 }}>
-              <Text>hi</Text>
-              <Text>hi</Text>
-              <Text>hi</Text>
-              <Text>hi</Text>
-            </View>
-          </View>
-=======
               <View style={styles.ingredientsListPlus}>
                 <Button type="clear" icon={<AntDesign name="plus" size={24} color="black" />} onPress={this.addOverlay}></Button>
               </View>
@@ -269,29 +193,11 @@ class Refrigerator extends Component<RefrigeratorProps, RefrigeratorState> {
 
 
 
->>>>>>> feature/S03P31D204-110/refrigerator_add_modal
           <Overlay
             fullScreen={true}
             isVisible={addVisible}
             onBackdropPress={this.addOverlay}
           >
-<<<<<<< HEAD
-            <View style={{ flex: 1, flexDirection: 'column' }}>
-              <View style={{ flex: 1, justifyContent: 'space-around', alignItems: 'center', flexDirection: 'row' }}>
-                <View style={{ flex: 1 }}></View>
-                <View style={{ flex: 6, justifyContent: 'center', alignItems: 'center' }}><Text>제품등록</Text></View>
-                <View style={{ flex: 1, justifyContent: 'flex-end' }}><Button type='clear' icon={<Entypo name="camera" size={24} color="black" />}></Button></View>
-              </View>
-              <Text style={{ flex: 8, alignItems: 'center' }}>hi, im overlay</Text>
-              <View style={{ flex: 1, justifyContent: 'space-around', flexDirection: 'row' }}>
-                <Button type='outline' style={{ flex: 4 }} onPress={this.addOverlay} title='닫기'></Button>
-                <Button type='outline' style={{ flex: 4 }} onPress={this.addOverlay} title='저장'></Button>
-              </View>
-            </View>
-          </Overlay>
-        </KeyboardAwareScrollView>
-      </View >
-=======
             <View style={styles.overlay}>
 
               <View style={styles.overlayHeader}>
@@ -313,14 +219,10 @@ class Refrigerator extends Component<RefrigeratorProps, RefrigeratorState> {
           </Overlay>
         </View >
       </KeyboardAwareScrollView>
->>>>>>> feature/S03P31D204-110/refrigerator_add_modal
     );
   }
 }
 
-<<<<<<< HEAD
-export default Refrigerator;
-=======
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -446,4 +348,3 @@ const mapDispatchToProps = (dispatch: any) => ({
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(Refrigerator)
->>>>>>> feature/S03P31D204-110/refrigerator_add_modal
