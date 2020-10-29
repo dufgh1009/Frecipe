2ecb64f8e3953a09640d84349d20398905a5041d
엄강우 <yhnb33@gmail.com>
2020-10-29 16:21:46 +0900

    S03P31D204-114 S03P31D204[+]filter search

--- frontend/Frecipe/redux/refrigeratorSlice.tsx
+++ frontend/Frecipe/redux/refrigeratorSlice.tsx
@@ -1,5 +1,9 @@
 const ADD = 'refrigerator/ADD' as const;
 const DELETE = 'refrigerator/DELETE' as const;
+const DELETEALL = 'refrigerator/DELETEALL' as const;
+const SEARCH = 'refrigerator/SEARCH' as const;
+const MAXID = 'refrigerator/MAXID' as const;
+const ORDER = 'refrigerator/ORDER' as const;
 
 export interface ingredient {
   id: number;
@@ -11,41 +15,54 @@ export interface ingredient {
 
 
 export const add = (newIns: Array<ingredient>) => ({ type: ADD, payload: newIns });
-export const deleteAll = () => ({ type: DELETE });
+export const deleteIngredient = (id: number) => ({ type: DELETE, payload: id });
+export const deleteAll = () => ({ type: DELETEALL });
+export const search = (keyword: string) => ({ type: SEARCH, payload: keyword });
+export const increaseMaxId = () => ({ type: MAXID });
+export const order = (filter: string) => ({ type: ORDER, payload: filter });
 
 
-type Actions = ReturnType<typeof add> | ReturnType<typeof deleteAll>;
+
+type Actions = ReturnType<typeof add>
+  | ReturnType<typeof deleteIngredient>
+  | ReturnType<typeof deleteAll>
+  | ReturnType<typeof search>
+  | ReturnType<typeof increaseMaxId>
+  | ReturnType<typeof order>;
 
 type RefrigeratorState = {
   ingredients: Array<ingredient>,
+  searchIngredients: Array<ingredient>,
   maxId: number,
   yellowFood: number,
   redFood: number,
-  orderedIngredients: Array<ingredient>
 }
 const initialState: RefrigeratorState = {
   ingredients: [],
+  searchIngredients: [],
   maxId: 0,
   yellowFood: 0,
   redFood: 0,
-  orderedIngredients: []
 };
 
 function refrigerator(
   state: RefrigeratorState = initialState,
   action: Actions
 ): RefrigeratorState {
+  const ingredients = Object.assign([], state.ingredients)
   switch (action.type) {
     case ADD:
-      var newIngredients = Object.assign([], state.ingredients)
       var maxId = 0
       var yCount = 0
       var rCount = 0
+      var newIngredients = Object.assign([], state.ingredients)
       for (var element of action.payload) {
+        if (element.name === "" || element.count === 0 || element.date < 0) {
+          continue
+        }
         newIngredients.push(element);
         maxId = element.id
       }
-
       newIngredients.map((element: ingredient) => {
         if (element.date <= 3 && element.date >= 1) {
           yCount = yCount + 1
@@ -53,24 +70,66 @@ function refrigerator(
           rCount = rCount + 1
         }
       })
-
-      var orderByExperationDateList = Object.assign([], newIngredients)
-      orderByExperationDateList.sort(function (a: ingredient, b: ingredient) {
-        if (a.date > b.date) {
-          return 1;
-        }
-        if (a.date < b.date) {
-          return -1;
-        }
-        return 0;
-      });
-      return Object.assign({}, state, { orderedIngredients: orderByExperationDateList, yellowFood: yCount, redFood: rCount, maxId: maxId, ingredients: newIngredients });
+      return Object.assign({}, state, { yellowFood: yCount, redFood: rCount, maxId: maxId + 1, ingredients: newIngredients, searchIngredients: newIngredients });
     case DELETE:
-      const deleteIngredients: ingredient[] = []
-      return Object.assign({}, state, { orderedIngredients: deleteIngredients, yellowFood: 0, redFood: 0, maxId: 0, ingredients: deleteIngredients });
+      var deleteIngredients: ingredient[] = []
+      ingredients.map((element: ingredient) => {
+        if (element.id != action.payload) {
+          deleteIngredients.push(element)
+        }
+      })
+      var yCount = 0
+      var rCount = 0
+      deleteIngredients.map((element: ingredient) => {
+        if (element.date <= 3 && element.date >= 1) {
+          yCount = yCount + 1
+        } else if (element.date < 1) {
+          rCount = rCount + 1
+        }
+      })
+      return Object.assign({}, state, { ingredients: deleteIngredients, yellowFood: yCount, redFood: rCount, searchIngredients: deleteIngredients });
+    case SEARCH:
+      var searchIngredient: ingredient[] = []
+      ingredients.map((element: ingredient) => {
+        if (element.name.includes(action.payload)) {
+          searchIngredient.push(element)
+        }
+      })
+      return Object.assign({}, state, { searchIngredients: searchIngredient })
+    case DELETEALL:
+      return Object.assign({}, state, { ingredients: [], searchIngredients: [], yellowFood: 0, redFood: 0, maxId: 0 });
+    case MAXID:
+      return Object.assign({}, state, { maxId: state.maxId + 1 })
+    case ORDER:
+      if (action.payload === 'update') {
+        var orderByUpdateDateList = Object.assign([], state.searchIngredients)
+        orderByUpdateDateList.sort(function (a: ingredient, b: ingredient) {
+          if (a.id > b.id) {
+            return 1;
+          }
+          if (a.id < b.id) {
+            return -1;
+          }
+          return 0;
+        });
+        return Object.assign({}, state, { searchIngredients: orderByUpdateDateList })
+      } else {
+        var orderByExperationDateList = Object.assign([], state.searchIngredients)
+        orderByExperationDateList.sort(function (a: ingredient, b: ingredient) {
+          if (a.date > b.date) {
+            return 1;
+          }
+          if (a.date < b.date) {
+            return -1;
+          }
+          return 0;
+        });
+        return Object.assign({}, state, { searchIngredients: orderByExperationDateList })
+      }
     default:
       return state;
   }
 }
+
 
 export default refrigerator;
