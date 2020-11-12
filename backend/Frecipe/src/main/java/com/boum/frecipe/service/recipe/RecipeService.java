87a45a9480c6ecb39d6ae3cb43bfebadab5f282f
yeoro <dufgh1009@naver.com>
2020-11-12 09:53:12 +0900

    [+] Edit ingredient entity

--- backend/Frecipe/src/main/java/com/boum/frecipe/service/recipe/RecipeService.java
+++ backend/Frecipe/src/main/java/com/boum/frecipe/service/recipe/RecipeService.java
@@ -2,6 +2,8 @@ package com.boum.frecipe.service.recipe;
 
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.boum.frecipe.domain.recipe.Recipe;
 import com.boum.frecipe.dto.recipe.RecipeDTO;
 
@@ -9,6 +11,9 @@ public interface RecipeService {
 
 	// 레시피 등록
 	public Recipe addRecipe(String username, RecipeDTO recipeDto);
+	
+	// 레시피 이미지 등록
+	public String uploadImages(String title, MultipartFile image);
 	
 	// 레시피 상세 조회
 	public Recipe retrieve(Long recipeNo);
