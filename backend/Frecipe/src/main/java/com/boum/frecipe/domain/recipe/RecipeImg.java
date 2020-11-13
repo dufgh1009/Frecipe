701a98a0ca93e285e8459f3059dd0bb21ad27453
yeoro <dufgh1009@naver.com>
2020-11-13 15:32:44 +0900

    [+] Edit recipe

---
+++ backend/Frecipe/src/main/java/com/boum/frecipe/domain/recipe/RecipeImg.java
@@ -0,0 +1,13 @@
+package com.boum.frecipe.domain.recipe;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecipeImg {
+	
+	private String image;
+}
